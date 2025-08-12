ad6eb60cff75b4078b437da6587a2f281f8106e2
Poshit Rathi <poshitrathi03@gmail.com>
2025-08-12 20:04:27 +0530

    cokkie issue update

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,14 +1,14 @@
 export const sendToken = (user, statusCode, res, message) => {
   const token = user.getJWTToken();
-  
+
   // Set cookie options
   const options = {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production', // true in production
-    sameSite: 'lax',
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
     path: '/',
   };
 
